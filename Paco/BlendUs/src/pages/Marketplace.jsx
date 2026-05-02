e16d286223471c2cbbc033f47f3f31599b5aea27
fsermun3103 <fsermun3103@ieszaidinvergeles.org>
2026-05-02 13:10:23 +0200

    cambios Marketplace

--- Paco/BlendUs/src/pages/Marketplace.jsx
+++ Paco/BlendUs/src/pages/Marketplace.jsx
@@ -19,9 +19,410 @@ function Marketplace() {
         <p className="subtitle">Discover amazing smoothies tools from creators around the world.</p>
       </div>
       <section className="products-grid">
-        <div className="grid">
-        
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
 
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
+        </div>
+          <div className="product-card">
+            <img src="public/assets/smoothie.avif" className="product-image"/>
+
+            <div className="product-content">
+                <span className="product-category">
+                    Kitchen tools
+                </span>
+
+                <h3 className="product-title">
+                    Turbo Blender
+                </h3>
+
+                <p className="product-description">
+                    The true power of Turbo Blender lies in its ability to effortlessly crush even the toughest ingredients.
+                </p>
+
+                <div className="product-rating">
+                    <div className="stars">
+                        {'★★★★★'}
+                    </div>
+
+                    <span className="rating-text">
+                        4.5 (200)
+                    </span>
+                </div>
+
+                <div className="product-footer">
+                    <span className="product-price">
+                        € 19.99
+                    </span>
+
+                    <button className="add-btn">
+                        <svg
+                            viewBox="0 -960 960 960"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM160-120q-33 0-56.5-23.5T80-200v-560h80v560h560v80H160Zm160-160q-33 0-56.5-23.5T240-360v-440q0-33 23.5-56.5T320-880h440q33 0 56.5 23.5T840-800v440q0 33-23.5 56.5T760-280H320Zm0-80h440v-440H320v440Zm0 0v-440 440Z"/>
+                        </svg>
+
+                        Añadir
+                    </button>
+                </div>
+            </div>
         </div>
         
       </section>
